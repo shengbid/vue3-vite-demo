f42a49371b8efec5c3d908525441d776caa8da57
shengbid <441974767@qq.com>
2023-03-12 21:41:35 +0800

    路由拦截增加角色菜单控制

--- src/router/author.js
+++ src/router/author.js
@@ -1,18 +1,21 @@
 // 路由鉴权
 
-import router from './index'
-
+import router from "./index";
+import { getRoleAuthor } from "./roleMenu";
 
 router.beforeEach((to, from) => {
   const loginInfo = JSON.parse(sessionStorage.getItem("loginInfo")) || {};
-  const {
-    userName
-  } = loginInfo;
-  const noLogin = userName ? false : true
-  if (noLogin && to.name !== 'login') {
+  const { userName } = loginInfo;
+  const noLogin = userName ? false : true;
+  if (noLogin && to.name !== "login") {
     return {
-      name: 'login'
-    }
+      name: "login",
+    };
   }
 
-})
+  if (!getRoleAuthor(userName, to.path) && to.name !== "403") {
+    return {
+      name: "403",
+    };
+  }
+});
