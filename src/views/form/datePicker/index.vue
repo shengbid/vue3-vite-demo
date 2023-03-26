62c5a90925bff3ee6de0467fea2b86cd896b69c5
shengbid <441974767@qq.com>
2023-03-26 21:00:13 +0800

    日期组件

---
+++ src/views/form/datePicker/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <a-space :size="20">
+      <a-date-picker v-model:value="date1" />
+    </a-space>
+  </div>
+</template>
+
+<script>
+import { reactive, toRefs } from "vue";
+import moment from "moment";
+
+export default {
+  setup() {
+    const dateInfo = reactive({
+      date1: moment("2023-02-02"),
+    });
+    return {
+      ...toRefs(dateInfo),
+    };
+  },
+};
+</script>
