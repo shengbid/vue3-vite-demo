ef93e38683457b2bbd6efac7eee32413517a5451
shengbid <441974767@qq.com>
2023-03-26 20:59:55 +0800

    修改组件传参文件

---
+++ src/views/vueinfo/params/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    home
+    <a-button @click="editName">修改parentname</a-button>
+    <Child :title="title" :age="age" @click="clickChild" />
+
+    <div>
+      <CardA />
+      <CardB />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Child from "./components/child.vue";
+import { ref, provide } from "vue";
+import CardA from "./components/cardA.vue";
+import CardB from "./components/cardB.vue";
+
+const title = ref("父组件传递的title");
+const age = ref(17);
+const parentName = ref("parentName");
+
+provide("parentName", parentName);
+
+const clickChild = (val) => {
+  console.log(val);
+};
+const editName = () => {
+  parentName.value = "parentName" + new Date();
+};
+</script>
