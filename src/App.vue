2a4067050fb8a95ee4c7fc8fcb35ba0e73e0218c
shengbid <441974767@qq.com>
2023-04-01 20:54:46 +0800

    日期组件国际化设置

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from "./components/HelloWorld.vue";
 import zhCN from "ant-design-vue/es/locale/zh_CN";
+import dayjs from "dayjs";
+import "dayjs/locale/zh-cn";
+dayjs.locale("zh-cn");
 
 const locale = zhCN;
 </script>
